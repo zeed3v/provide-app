2a44e68e2cb192da5df6b0e5365fb48c4cf39537
zeed3v <felipebrionese@gmail.com>
2022-04-20 19:26:34 -0400

    commit

---
+++ src/components/ContadorAccion.vue
@@ -0,0 +1,23 @@
+<template>
+  <button @click="contador ++">Aumentar</button>
+  <button @click="disminuir">Disminuir</button>
+</template>
+
+<script>
+import { inject } from '@vue/runtime-core'
+export default {
+    setup(){
+        const contador = inject('contador')
+
+        const disminuir = () => {
+            contador.value --
+        }
+
+        return {contador, disminuir}
+    }
+}
+</script>
+
+<style>
+
+</style>
